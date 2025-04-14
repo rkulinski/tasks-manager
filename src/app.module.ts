da997aac8449e0a10eb84e73287767c62912ecb0
robert <robert.kulinski93@gmail.com>
2025-04-14 18:48:42 +0200

    remove unused app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [TasksModule, HealthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
