512c0e249a59bd7874f8dd339b9be5fc7821f3be
robert <robert.kulinski93@gmail.com>
2025-04-15 16:28:28 +0200

    expose filters to swagger

--- src/tasks/dto/filter.dto.ts
+++ src/tasks/dto/filter.dto.ts
@@ -1,11 +1,14 @@
 import { IsEnum, IsOptional, IsUUID } from 'class-validator';
 import { TaskStatus } from './create-task.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class FilterTasksDto {
+  @ApiPropertyOptional()
   @IsOptional()
   @IsUUID()
   userId?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsEnum(TaskStatus)
   status?: TaskStatus;
